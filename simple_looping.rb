0cabbfd35d84865bbdbdc013c07c9efb4dfc33ed
Max Sun <max.sun@yale.edu>
2019-05-15 05:29:44 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -33,6 +33,7 @@ def until_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 0
   until counter == number_of_times
+    counter += 1
     puts phrase
   end
 end
